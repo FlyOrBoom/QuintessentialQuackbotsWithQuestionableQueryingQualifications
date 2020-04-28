5b2f425895e7f62516246909d80ed2b666c3078b
LXing <31298876+FlyOrBoom@users.noreply.github.com>
2020-04-28 15:08:39 -0700

    shorten delay, reenable keyword filtering

--- Qurious/qurious.py
+++ Qurious/qurious.py
@@ -1,28 +1,12 @@
+import os, discord, requests
 from dotenv import load_dotenv
-load_dotenv()
 
-import os
-import discord
-import requests
-
-mdn='https://developer.mozilla.org/'
-client = discord.Client(
-	activity=discord.CustomActivity('Query MDN: .q / .qjs / .qcs / etc')
-)
+mdn = 'https://developer.mozilla.org/'
+digits = ['0️⃣', '1️⃣','2️⃣','3️⃣', '4️⃣','5️⃣','6️⃣', '7️⃣','8️⃣','9️⃣']
 active_ids = []
 active_documents = []
-digits = [
-	'0️⃣',
-	'1️⃣',
-	'2️⃣',
-	'3️⃣',
-	'4️⃣',
-	'5️⃣',
-	'6️⃣',
-	'7️⃣',
-	'8️⃣',
-	'9️⃣'
-]
+load_dotenv()
+client = discord.Client()
 
 @client.event
 async def on_ready():
@@ -30,23 +14,19 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-
-	if message.content.startswith('.q '):
+	params = message.content.split(' ',1)
+	if params[0].lower() in ['.m','.mdn']:
 		async with message.channel.typing():
 			documents = requests.get(
-				f"{mdn}api/v1/search/en-US?q={message.content[3:]}&highlight=false"
+				mdn + 'api/v1/search/en-US?highlight=false&q=' + params[1]
 			).json()['documents']
-
 			reply = await message.channel.send(
 				embed = await embed(documents[0])
 			)
-
 		for digit in digits:
 			await reply.add_reaction(digit)
-
 		active_ids.append(reply.id)
 		active_documents.append(documents)
-
 		if len(active_ids) > 10:
 			del active_ids[0]
 			del active_documents[0]
@@ -69,16 +49,16 @@ async def on_reaction_add(new_reaction, user):
 
 async def embed(document):
 	return discord.Embed(
-		title=document['title'],
-		url=f"{mdn}en-US/docs/{document['slug']}",
-		description=document['excerpt'],
-		color=0x83d0f2
+		title =       document['title'],
+		url =         mdn + 'en-US/docs/' + document['slug'],
+		description = document['excerpt'],
+		color =       0x83d0f2
 	).set_author(
-		name='ᴍᴅɴ',
-		url=f"{mdn}en-US/docs/Web",
-		icon_url=f"{mdn}static/img/favicon32.7f3da72dcea1.png"
+		name =       'ᴍᴅɴ',
+		url =         mdn + 'en-US/docs/Web',
+		icon_url =    mdn + 'static/img/favicon32.7f3da72dcea1.png'
 	).set_footer(
-		text='Click the numbers below to view other results'
+		text =        'Click the numbers below to view other results'
 	)
 
 client.run(os.environ['TOKEN'])

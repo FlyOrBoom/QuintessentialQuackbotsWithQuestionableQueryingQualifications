46760e07995756ab79c6e41a4a3e4db8923df349
LXing <31298876+FlyOrBoom@users.noreply.github.com>
2020-04-28 01:50:45 -0700

    Fetches MDN's actual summaries through additional requests instead of the search excerpts. Takes a bit more time to send out the message (and to debug jesus christ), but it's worth it. Also convets HTML into Discord markup.

--- Qurious/qurious.py
+++ Qurious/qurious.py
@@ -7,9 +7,12 @@ import requests
 import re
 import textwrap
 
-client = discord.Client()
+mdn='https://developer.mozilla.org/'
+client = discord.Client(
+	activity=discord.CustomActivity('Query MDN: .q / .qjs / .qcs / etc')
+)
 active_ids = []
-active_results = []
+active_documents = []
 digits = [
 	'0️⃣',
 	'1️⃣',
@@ -31,37 +34,41 @@ async def on_ready():
 async def on_message(message):
 
 	match = re.match(
-		'q(|css|canvas|html|http|js|svg|webdev|standards|webext|webgl)\s(.*)',
+		'.mdn(|css|canvas|html|http|js|svg|webdev|standards|webext|webgl)\s(.*)',
 		message.content
 	)
 
 	if match:
-		results = requests.get(
-			f'https://developer.mozilla.org/api/v1/search/en-us?q={match.group(0)}&topic={match.group(1)}'
-		).json()['documents']
+		async with message.channel.typing():
+			documents = requests.get(
+				f"{mdn}api/v1/search/en-US?q={match.group(0)}&topic={match.group(1)}"
+			).json()['documents']
 
-		reply = await message.channel.send(
-			embed = await embed(results[0])
-		)
+			for document in documents:
+				document['url']=f"{mdn}en-US/docs/{document['slug']}"
+
+			reply = await message.channel.send(
+				embed = await embed(documents[0])
+			)
 
 		for digit in digits:
 			await reply.add_reaction(digit)
 
 		active_ids.append(reply.id)
-		active_results.append(results)
+		active_documents.append(documents)
 
 		if len(active_ids) > 10:
 			del active_ids[0]
-			del active_results[0]
+			del active_documents[0]
 
 @client.event
-async def on_reaction_add(reaction, user):
+async def on_reaction_add(new_reaction, user):
 	if (user != client.user
 		and new_reaction.message.id in active_ids
 		and new_reaction.emoji in digits):
 		await new_reaction.message.edit(
 			embed = await embed(
-				active_results[active_ids.index(new_reaction.message.id)][digits.index(new_reaction.emoji)]
+				active_documents[active_ids.index(new_reaction.message.id)][digits.index(new_reaction.emoji)]
 			)
 		)
 		for reaction in new_reaction.message.reactions:
@@ -70,21 +77,25 @@ async def on_reaction_add(reaction, user):
 					if user != client.user:
 						await reaction.remove(user)
 
-async def embed(result):
+async def embed(document):
 	return discord.Embed(
-		title=result['title'],
-		url='https://developer.mozilla.org/en-US/'+result['slug'],
-		description=re.sub(
-			r'<(|/)mark>',
-			'',
-			textwrap.shorten(result['excerpt'],width=256,placeholder='..').capitalize()+'.'
-		),
-		footer='⬇️ flip through results ⬇️',
+		title=document['title'],
+		url=document['url'],
+		description=(
+			re.sub(r"<.+?>",'',
+			re.sub(r"<\/?pre>",'```',
+			re.sub(r"<\/?code>",'`',
+			re.sub(r"<\/?em>",'*',
+			re.sub(r"<\/?strong>",'**',
+			requests.get(f"{document['url']}?summary&raw").text
+		)))))),
 		color=0x83d0f2
 	).set_author(
 		name='ᴍᴅɴ',
-		url='https://developer.mozilla.org/en-US',
-		icon_url='https://developer.mozilla.org/static/img/favicon32.7f3da72dcea1.png'
+		url=f"{mdn}en-US/docs/Web",
+		icon_url=f"{mdn}static/img/favicon32.7f3da72dcea1.png"
+	).set_footer(
+		text='Click the numbers below to view other results'
 	)
 
 client.run(os.environ['TOKEN'])
